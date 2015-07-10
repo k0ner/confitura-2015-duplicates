860f2db751996dbb22e3a7faa0d8b8bc547588fa
Konrad Tendera <konradtendera@gmail.com>
2015-07-10 22:56:23 +0200

    comments added

--- src/main/scala/duplicates/Equalable.scala
+++ src/main/scala/duplicates/Equalable.scala
@@ -1,5 +1,6 @@
 package duplicates
 
+// just to have the constraint for custom classes
 trait Equalable {
   def equals: Boolean
 }
--- src/main/scala/duplicates/Hashable.scala
+++ src/main/scala/duplicates/Hashable.scala
@@ -1,5 +1,6 @@
 package duplicates
 
+// just to have the constraint for custom classes
 trait Hashable {
   def hashCode: Int
 }
